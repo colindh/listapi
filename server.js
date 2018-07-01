75f6d289a17fb58c5ca9b8fbf7dfe4add5b22301
Colin Hume <colindh@gmail.com>
2018-06-30 23:38:50 -0500

    copy new starter app

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
-var express = require('express');
-
 var server = (function(){
+  var express = require('express'),
+  app = express();
 
-    var app = express(),
-		  http = require('http');
+  app.use(express.static('public'));
 
-    app.use(express.static('public'));
+  // accept GET request
+  app.get('/', function (req, res) {
+    res.send('Got a GET request');
+  });
 
-    // accept GET request
-    app.get('/', function (req, res) {
-	res.send('Got a GET request');
-    });
+  // accept POST request
+  app.post('/', function (req, res) {
+    res.send('Got a POST request');
+  });
 
-    // accept POST request
-    app.post('/', function (req, res) {
-	res.send('Got a POST request');
-    });
+  // accept PUT request
+  app.put('/', function (req, res) {
+    res.send('Got a PUT request');
+  });
 
-    // accept PUT request
-    app.put('/', function (req, res) {
-	res.send('Got a PUT request');
-    });
-
-    // accept DELETE request
-    app.delete('/', function (req, res) {
-	res.send('Got a DELETE request');
-    });
+  // accept DELETE request
+  app.delete('/', function (req, res) {
+    res.send('Got a DELETE request');
+  });
 
 
-	 app.listen(3000, function () {
-		  console.log('App server listening on port 3000!');
-	 });
 })();
